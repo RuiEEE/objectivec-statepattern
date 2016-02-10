f2ac99c45457bb25f8abfdc07cbef55723ea8791
rsantos <ruiapdsantos@gmail.com>
2016-02-10 09:56:33 +0000

    Initial Commit

---
+++ StatePattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StatePattern
+//
+//  Created by Rui Santos on 10/02/16.
+//  Copyright © 2016 Rui Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
